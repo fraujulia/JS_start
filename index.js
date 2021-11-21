a6c1cec777147d057251f8a5c90bafc20d29d1ee
yulia tertyshnyk <yulia.tertyshnyk@gmail.com>
2021-11-21 12:18:57 +0200

    added JS

---
+++ index.js
@@ -0,0 +1,7 @@
+let promptResponse = prompt ('What is you name?');
+alert ('Hello ' + promptResponse + '! How are you?');
+
+//let promptResponse = prompt ('What is you name?');
+//alert (`Hello ${promptResponse}! How are you?`);
+
+
